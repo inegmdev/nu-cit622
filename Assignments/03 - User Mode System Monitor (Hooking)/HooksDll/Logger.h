fb6f6f09b29e0c6010deb52709002fdfdfb770c2
Islam NEGM <i.g.negm@gmail.com>
2023-06-19 01:07:50 +0300

    Remove unnecessary commented code

--- Assignments/03 - User Mode System Monitor (Hooking)/HooksDll/Logger.h
+++ Assignments/03 - User Mode System Monitor (Hooking)/HooksDll/Logger.h
@@ -2,7 +2,6 @@
 #include <string>
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/fmt.h"
-#include "spdlog/fmt/bundled/xchar.h"
 
 class Logger {
 private:
@@ -12,29 +11,9 @@ public:
 	Logger();
 	void init();
     void deinit();
-#if 0
+
 	template<typename... Args>
 	void write(fmt::format_string<Args...> fmt, Args &&... args) {
 		m_logger->info(fmt, std::forward<Args>(args)...);
 	}
-
-    template<typename... Args>
-    void write(fmt::wformat_string<Args...> fmt, Args &&... args) {
-        std::wstring formattedString = fmt::format(fmt, std::forward<Args>(args)...);
-        m_logger->info("{}", formattedString);
-    }
-#else
-    template<typename... Args>
-    void write(fmt::format_string<Args...> fmt, Args&&... args) {
-        m_logger->info(fmt, std::forward<Args>(args)...);
-    }
-#if 0
-    template<typename... Args>
-    void write(fmt::wstring_view fmt, Args&&... args) {
-        std::wstring formattedString = fmt::format(fmt, std::forward<Args>(args)...);
-        std::string convertedString(formattedString.begin(), formattedString.end());
-        m_logger->info(convertedString);
-    }
-#endif
-#endif
 };
